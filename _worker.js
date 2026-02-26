e3ff23900e227c8f77a57fd344c15b084a942af8
Kurniawan Dwi Waestaputra <35242435+BlueHat358@users.noreply.github.com>
2026-02-26 15:23:09 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-let serviceName = "";
-let APP_DOMAIN = "";
+let serviceName = "gardevoir";
+let APP_DOMAIN = "bluehat358.workers.dev";
 
 let prxIP = "";
 let cachedPrxList = [];
